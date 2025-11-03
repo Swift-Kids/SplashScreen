9b72e09a960a0daba861bf30670e57b3484eecbb
Brendan <brendancmiller@gmail.com>
2025-11-03 13:09:15 -0500

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Brendan Miller on 2025-11-03.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
